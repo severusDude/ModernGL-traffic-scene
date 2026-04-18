19868116047bdd6d2e4745dca84ab208f71a8a3c
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2026-04-18 12:03:58 +0700

    fix(shader): change shader_program func and update default texture vertex shader

--- shader_program.py
+++ shader_program.py
@@ -3,25 +3,26 @@ class ShaderProgram:
     def __init__(self, ctx):
         self.ctx = ctx
         self.programs = {}
-        
+
         self.programs['default_color'] = self.get_program('default_color')
         self.programs['rough_color'] = self.get_program('rough_color')
 
         self.programs['default_texture'] = self.get_program('default_texture')
-        
+
         self.programs['skybox'] = self.get_program('skybox')
         self.programs['skybox_next'] = self.get_program('skybox_next')
         self.programs['shadow_map'] = self.get_program('shadow_map')
 
     # SETUP BUAT BACA SHADER (.frag, .vert)
     def get_program(self, shader_program_name):
-        with open(f'shaders/{shader_program_name}.vert') as file:
+        with open(f'shaders/{shader_program_name}.vert', encoding='utf-8') as file:
             vertex_shader = file.read()
 
-        with open(f'shaders/{shader_program_name}.frag') as file:
+        with open(f'shaders/{shader_program_name}.frag', encoding='utf-8') as file:
             fragment_shader = file.read()
 
-        program = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+        program = self.ctx.program(
+            vertex_shader=vertex_shader, fragment_shader=fragment_shader)
         return program
 
     def destroy(self):
